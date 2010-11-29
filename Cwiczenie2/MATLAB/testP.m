672fd2009720a787bb864d1084db658a7c0d61b4
Tomasz 'TC' Klimek <tc112233@gmail.com>
2010-11-29 22:35:52 +0100

    Koniec zadania 2... Mam nadzieje

--- Cwiczenie2/MATLAB/testP.m
+++ Cwiczenie2/MATLAB/testP.m
@@ -2,7 +2,6 @@ function testP(start, step, stop)
 load_system('model.mdl');
 hold on;
 k = start;
-i = 0;
 color = char('r', 'k', 'b', 'g', 'y', 'm');
 set_param('model/Gain1', 'Gain', num2str(0));
 set_param('model/Gain2', 'Gain', num2str(0));
@@ -12,11 +11,6 @@ while (k <= stop)
     wy = simout.signals.values;
     figure(1);
     plot(tout, wy, 'Color', color(mod(i,6)+1));
-    i = i + 1;
-    q(i) = sum(wy.^2)/length(wy);
-    ka(i) = k;
     k = k + step;
 end
-figure(2);
-plot(ka, q);
 end
