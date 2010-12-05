417c95fb8107d3cc705feb7780d874cad4206003
Dotevo <dotevo@gmail.com>
2010-12-05 17:42:43 +0100

    Zmodyfikowana funkcja test :-)

--- Cwiczenie3/MATLAB/test.m
+++ Cwiczenie3/MATLAB/test.m
@@ -1,18 +1,21 @@
-function test(start, step, stop)
-load_system('dwuBez.mdl');
+function test(start, step, stop, model,func)
+load_system(strcat(model,'.mdl'));
 hold on;
 k = start;
 i=0;
 legendtext{1}='';
 
 color = char('r', 'k', 'b', 'g', 'y', 'm');
-set_param('dwuBez/Gain', 'Gain', num2str(0));
+set_param(strcat(model,'/Gain'), 'Gain', num2str(0));
 
 while (k <= stop)
     i=i+1;
-    set_param('dwuBez/Gain', 'Gain', num2str(k));
-    sim('dwuBez.mdl');
-    wy = simout.signals.values;
+    set_param(strcat(model,'/Gain'), 'Gain', num2str(k));
+    sim(strcat(model,'.mdl'));
+    wy= u.signals.values;;
+    if(func=='e')
+        wy = e.signals.values;    
+    end
     figure(1);
     plot(tout, wy, 'Color', color(mod(i,6)+1));        
     legendtext{i}= strcat('K=' , num2str(k));
