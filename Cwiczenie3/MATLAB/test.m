731e71c5a832aafbae28a33640957b457b14ba83
Dotevo <dotevo@gmail.com>
2010-12-07 00:18:11 +0100

    test.m (zmiana a symetryczna)

--- Cwiczenie3/MATLAB/test.m
+++ Cwiczenie3/MATLAB/test.m
@@ -1,31 +1,29 @@
-function test(start, step, stop, model,func)
+function test(start1,start2, step, stop, model)
 load_system(strcat(model,'.mdl'));
 hold on;
-k = start;
 i=0;
 color = char('r', 'k', 'b', 'g', 'y', 'm');
 
-set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(0));
-set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(0));
+set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(-30000));
+set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(100000));
 
-while (k <= stop)
+while (start2+step*i <= stop)
     i=i+1;
-    set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(k+1));
-    set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(k));
+    set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(start1+step*i));
+    set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(start2-step*i));
     
     sim(strcat(model,'.mdl'));
     figure(1);
     wy= u.signals.values;    
     plot(tout, wy, 'Color', color(mod(i,6)+1));        
-    legendtext{i}= strcat('K=' , num2str(k));
+    legendtext{i}= strcat('a=' , num2str(start1+step*i-start2+step*i));
     legend(legendtext);
     
     figure(2);
     wy= e.signals.values;    
     plot(tout, wy, 'Color', color(mod(i,6)+1));        
-    legendtext2{i}= strcat('K=' , num2str(k));
+    legendtext2{i}= strcat('a=' , num2str(start1+step*i-start2+step*i));
     legend(legendtext2);
-    k = k + step;    
     hold all;
 end
 
