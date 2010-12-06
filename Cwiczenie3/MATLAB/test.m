ddb2e72746777647ca16e63e2204feb04b88e795
Dotevo <dotevo@gmail.com>
2010-12-07 00:01:25 +0100

    POprawka w funkcji

--- Cwiczenie3/MATLAB/test.m
+++ Cwiczenie3/MATLAB/test.m
@@ -3,25 +3,30 @@ load_system(strcat(model,'.mdl'));
 hold on;
 k = start;
 i=0;
-legendtext{1}='';
-
 color = char('r', 'k', 'b', 'g', 'y', 'm');
-set_param(strcat(model,'/Gain'), 'Gain', num2str(0));
+
+set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(0));
+set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(0));
 
 while (k <= stop)
     i=i+1;
-    set_param(strcat(model,'/Gain'), 'Gain', num2str(k));
+    set_param(strcat(model,'/Relay1'), 'On_switch_value', num2str(k+1));
+    set_param(strcat(model,'/Relay1'), 'Off_switch_value', num2str(k));
+    
     sim(strcat(model,'.mdl'));
-    wy= u.signals.values;;
-    if(func=='e')
-        wy = e.signals.values;    
-    end
     figure(1);
+    wy= u.signals.values;    
     plot(tout, wy, 'Color', color(mod(i,6)+1));        
     legendtext{i}= strcat('K=' , num2str(k));
     legend(legendtext);
-    hold all;     
+    
+    figure(2);
+    wy= e.signals.values;    
+    plot(tout, wy, 'Color', color(mod(i,6)+1));        
+    legendtext2{i}= strcat('K=' , num2str(k));
+    legend(legendtext2);
     k = k + step;    
+    hold all;
 end
 
 end
