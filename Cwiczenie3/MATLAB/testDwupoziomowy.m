4cb0bcf5d8350591fa393d0ce9d2a9bd5e8cfedb
Tomasz 'TC' Klimek <tc112233@gmail.com>
2010-12-07 19:42:53 +0100

    Regulator dwupoziomowy bez i z histereza

--- Cwiczenie3/MATLAB/testDwupoziomowy.m
+++ Cwiczenie3/MATLAB/testDwupoziomowy.m
@@ -1,37 +1,56 @@
-function test(step, stop, flag)
+function testDwupoziomowy(step, stop, flag)
 load_system('dwupoziomowy.mdl');
 hold on;
 i=1;
 color = char('y', 'k', 'b', 'g', 'r', 'm');
 
-set_param('dwupoziomowy/Relay1', 'Off_switch_value', num2str(-step));
-set_param('dwupoziomowy/Relay1', 'On_switch_value', num2str(step));
+if step ~= 0
+    while (step*i <= stop)
+        set_param('dwupoziomowy/Relay1', 'On_switch_value', num2str(step*i));
+        set_param('dwupoziomowy/Relay1', 'Off_switch_value', num2str(-step*i));
 
-while (step*i <= stop)
-    set_param('dwupoziomowy/Relay1', 'On_switch_value', num2str(step*i));
-    set_param('dwupoziomowy/Relay1', 'Off_switch_value', num2str(-step*i));
-    
+        sim('dwupoziomowy.mdl');
+        figure(1);
+        uwy= u.signals.values;    
+        plot(tout, uwy, 'Color', color(mod(i,6)+1));        
+        %legendtext1{i}= strcat('a=' , num2str(step*i));
+        %legend(legendtext1);
+
+        figure(2);
+        ewy= e.signals.values;    
+        plot(tout, ewy, 'Color', color(mod(i,6)+1));        
+        %legendtext2{i}= strcat('a=' , num2str(step*i));
+        %legend(legendtext2);
+
+        if flag
+            hold on;
+            plot([0 30], [0 0], '-r');
+            plot([0 30], [step*i step*i], strcat('--', color(mod(i,6)+1)));
+            plot([0 30], [-step*i -step*i], strcat('--', color(mod(i,6)+1)));
+        end
+
+        i=i+1;
+        hold all;
+    end
+else
+    set_param('dwupoziomowy/Relay1', 'On_switch_value', num2str(0));
+    set_param('dwupoziomowy/Relay1', 'Off_switch_value', num2str(0));
+
     sim('dwupoziomowy.mdl');
     figure(1);
     uwy= u.signals.values;    
-    plot(tout, uwy, 'Color', color(mod(i,6)+1));        
-    %legendtext1{i}= strcat('a=' , num2str(step*i));
-    %legend(legendtext1);
-    
+    plot(tout, uwy, '-k');
+
     figure(2);
-    ewy= e.signals.values;    
-    plot(tout, ewy, 'Color', color(mod(i,6)+1));        
-    %legendtext2{i}= strcat('a=' , num2str(step*i));
-    %legend(legendtext2);
     
     if flag
         hold on;
+        plot([0 30], [step*i step*i], '--k');
+        plot([0 30], [-step*i -step*i], '--k');
         plot([0 30], [0 0], '-r');
-        plot([0 30], [step*i step*i], strcat('--', color(mod(i,6)+1)));
-        plot([0 30], [-step*i -step*i], strcat('--', color(mod(i,6)+1)));
     end
-    
-    i=i+1;
-    hold all;
+
+    ewy= e.signals.values;    
+    plot(tout, ewy, '-k');
 end
 end
--- Cwiczenie3/MATLAB/testTrojpoziomowy.m
+++ Cwiczenie3/MATLAB/testTrojpoziomowy.m
@@ -1,44 +1,66 @@
-function test(n, step, stop, flag)
+function testTrojpoziomowy(n, step, stop, flag)
 load_system('trojpoziomowy.mdl');
 hold on;
 i=1;
 color = char('y', 'k', 'b', 'g', 'r', 'm');
 
-set_param('trojpoziomowy/Relay1', 'Off_switch_value', num2str(n-step));
-set_param('trojpoziomowy/Relay1', 'On_switch_value', num2str(n+step));
-set_param('trojpoziomowy/Relay2', 'Off_switch_value', num2str(-(n+step)));
-set_param('trojpoziomowy/Relay2', 'On_switch_value', num2str(-(n-step)));
+if step ~= 0
+    while (step*i <= stop)
+        set_param('trojpoziomowy/Relay1', 'Off_switch_value', num2str(n-step*i));
+        set_param('trojpoziomowy/Relay1', 'On_switch_value', num2str(n+step*i));
+        set_param('trojpoziomowy/Relay2', 'Off_switch_value', num2str(-(n+step*i)));
+        set_param('trojpoziomowy/Relay2', 'On_switch_value', num2str(-(n-step*i)));
 
-while (step*i <= stop)
-    set_param('trojpoziomowy/Relay1', 'Off_switch_value', num2str(n-step*i));
-    set_param('trojpoziomowy/Relay1', 'On_switch_value', num2str(n+step*i));
-    set_param('trojpoziomowy/Relay2', 'Off_switch_value', num2str(-(n+step*i)));
-    set_param('trojpoziomowy/Relay2', 'On_switch_value', num2str(-(n-step*i)));
-    
+        sim('trojpoziomowy.mdl');
+        figure(1);
+        uwy= u.signals.values;    
+        plot(tout, uwy, 'Color', color(mod(i,6)+1));        
+        %legendtext1{i}= strcat('a=' , num2str(step*i));
+        %legend(legendtext1);
+
+        figure(2);
+        ewy= e.signals.values;    
+        plot(tout, ewy, 'Color', color(mod(i,6)+1));        
+        %legendtext2{i}= strcat('a=' , num2str(step*i));
+        %legend(legendtext2);
+
+        if flag
+            hold on;
+            plot([0 30], [n n], '-r');
+            plot([0 30], [-n -n], '-r');
+            plot([0 30], [n+step*i n+step*i], strcat('--', color(mod(i,6)+1)));
+            plot([0 30], [n-step*i n-step*i], strcat('--', color(mod(i,6)+1)));
+            plot([0 30], [-(n-step*i) -(n-step*i)], strcat('--', color(mod(i,6)+1)));
+            plot([0 30], [-(n+step*i) -(n+step*i)], strcat('--', color(mod(i,6)+1)));
+        end
+
+        i=i+1;
+        hold all;
+    end
+else
+    set_param('trojpoziomowy/Relay1', 'Off_switch_value', num2str(n));
+    set_param('trojpoziomowy/Relay1', 'On_switch_value', num2str(n));
+    set_param('trojpoziomowy/Relay2', 'Off_switch_value', num2str(-n));
+    set_param('trojpoziomowy/Relay2', 'On_switch_value', num2str(-n));
     sim('trojpoziomowy.mdl');
     figure(1);
     uwy= u.signals.values;    
-    plot(tout, uwy, 'Color', color(mod(i,6)+1));        
-    %legendtext1{i}= strcat('a=' , num2str(step*i));
-    %legend(legendtext1);
+    plot(tout, uwy, '-k');
     
     figure(2);
-    ewy= e.signals.values;    
-    plot(tout, ewy, 'Color', color(mod(i,6)+1));        
-    %legendtext2{i}= strcat('a=' , num2str(step*i));
-    %legend(legendtext2);
     
     if flag
         hold on;
+        plot([0 30], [n+step*i n+step*i], '--k');
+        plot([0 30], [n-step*i n-step*i], '--k');
+        plot([0 30], [-(n-step*i) -(n-step*i)], '--k');
+        plot([0 30], [-(n+step*i) -(n+step*i)], '--k');
         plot([0 30], [n n], '-r');
         plot([0 30], [-n -n], '-r');
-        plot([0 30], [n+step*i n+step*i], strcat('--', color(mod(i,6)+1)));
-        plot([0 30], [n-step*i n-step*i], strcat('--', color(mod(i,6)+1)));
-        plot([0 30], [-(n-step*i) -(n-step*i)], strcat('--', color(mod(i,6)+1)));
-        plot([0 30], [-(n+step*i) -(n+step*i)], strcat('--', color(mod(i,6)+1)));
     end
     
-    i=i+1;
-    hold all;
+    ewy= e.signals.values;    
+    plot(tout, ewy, '-k');  
+
 end
 end
