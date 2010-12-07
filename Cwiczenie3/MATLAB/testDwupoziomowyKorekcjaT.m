a41b5ce34cbfca65fb66529c2093b4fb32508a1c
Dotevo <dotevo@gmail.com>
2010-12-07 21:14:00 +0100

    dane funkcje do zmiany K i T

---
+++ Cwiczenie3/MATLAB/testDwupoziomowyKorekcjaT.m
@@ -0,0 +1,19 @@
+function testDwupoziomowyKorekcjaT(start,step, stop)
+load_system('dwupoziomowyKorekcja.mdl');
+hold on;
+i=1;
+color = char('y', 'k', 'b', 'g', 'r', 'm');
+
+while (start+step*i <= stop)
+        set_param('dwupoziomowyKorekcja/Transfer Fcn2', 'Denominator', strcat('[',num2str(start+step*i),' 1]'));
+        sim('dwupoziomowyKorekcja.mdl');
+        figure(1);
+        uwy= u.signals.values;    
+        plot(tout, uwy, 'Color', color(mod(i,6)+1));        
+        figure(2);
+        ewy= e.signals.values;    
+        plot(tout, ewy, 'Color', color(mod(i,6)+1));        
+        i=i+1;
+        hold all;
+end
+end
