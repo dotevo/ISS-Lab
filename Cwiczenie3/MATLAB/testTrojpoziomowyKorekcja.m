450ba98ec2508805b7663cd30aaa860244a30238
Tomasz 'TC' Klimek <tc112233@gmail.com>
2010-12-07 21:13:51 +0100

    PRAWIE koniec zadania 1
    Conflicts:
    
    	Cwiczenie3/MATLAB/dwupoziomowyKorekcja.mdl
    	Cwiczenie3/MATLAB/testTrojpoziomowyKorekcja.m

--- Cwiczenie3/MATLAB/testTrojpoziomowyKorekcja.m
+++ Cwiczenie3/MATLAB/testTrojpoziomowyKorekcja.m
@@ -5,14 +5,14 @@ i=1;
 color = char('y', 'k', 'b', 'g', 'r', 'm');
 
 while (start+step*i <= stop)
-        set_param('trojpoziomowyKorekcja/Constant', 'value', num2str(start+step*i));
-        sim('trojpoziomowyKorekcja.mdl');
+        set_param('dwupoziomowyKorekcja/Constant', 'value', num2str(start+step*i));
+        sim('dwupoziomowyKorekcja.mdl');
         figure(1);
-        uwy= u.signals.values;    
-        plot(tout, uwy, 'Color', color(mod(i,6)+1));        
+        uwy= u.signals.values;
+        plot(tout, uwy, 'Color', color(mod(i,6)+1));
         figure(2);
-        ewy= e.signals.values;    
-        plot(tout, ewy, 'Color', color(mod(i,6)+1));        
+        ewy= e.signals.values;
+        plot(tout, ewy, 'Color', color(mod(i,6)+1));
         i=i+1;
         hold all;
 end
