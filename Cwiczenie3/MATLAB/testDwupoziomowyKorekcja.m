b9ba8abdcea522eef2b0a01bf468cab412bf6e56
Dotevo <dotevo@gmail.com>
2010-12-07 20:23:58 +0100

    dwuziomowy z Korekcja

---
+++ Cwiczenie3/MATLAB/testDwupoziomowyKorekcja.m
@@ -0,0 +1,19 @@
+function testDwupoziomowyKorekcja(step, stop)
+load_system('dwupoziomowyKorekcja.mdl');
+hold on;
+i=1;
+color = char('y', 'k', 'b', 'g', 'r', 'm');
+
+while (step*i <= stop)
+        set_param('dwupoziomowyKorekcja/Constant', 'value', num2str(step*i));
+        sim('dwupoziomowyKorekcja.mdl');
+        figure(1);
+        uwy= u.signals.values;    
+        plot(tout, uwy, 'Color', color(mod(i,6)+1));        
+        figure(2);
+        ewy= e.signals.values;    
+        plot(tout, ewy, 'Color', color(mod(i,6)+1));        
+        i=i+1;
+        hold all;
+end
+end
