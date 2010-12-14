444d3e169a24dff9ac32bf37050daebafab7d199
Dotevo <dotevo@gmail.com>
2010-12-14 11:58:40 +0100

    Funkcja 3 :)

--- Cwiczenie4/MATLAB/test3.m
+++ Cwiczenie4/MATLAB/test3.m
@@ -1,26 +1,34 @@
-function test3(u,k,e,delta)      
+function test3(u,k,epsilon,delta)      
 
    %obliczanie pierwszych wartoœci :)
    u1(1) = u(1);
    u2(1) = u(2);   
    y(1) = funkcja1([u(1) u(2)]);
    %krok roboczy=delta
-   deltaM(1)=[delta 0];
-   deltaM(2)=[0 delta];
+   deltaM1=[delta 0];
+   deltaM2=[0 delta];
    
    n = 1;
    d1(1) = 1;
    d2(1) = 1;
+   e(1)=epsilon+1;
    %TODO
-   %while(epsilon < abs(d(n)))
-   %    d1(i+1)=funkcja1([u1(i) u2(i)]+deltaM(1))-funkcja1([u1(i) u2(i)]+deltaM(1));
-   %end
+   while(epsilon < abs(e(n)))
+       d1(n+1)=(funkcja1([u1(n) u2(n)]+deltaM1)-funkcja1([u1(n) u2(n)]-deltaM1))/delta;
+       d2(n+1)=(funkcja1([u1(n) u2(n)]+deltaM2)-funkcja1([u1(n) u2(n)]-deltaM2))/delta;
+       u1(n+1)=u1(n)-k*d1(n);
+       u2(n+1)=u2(n)-k*d2(n);
+       e(n+1)=d1(n+1)^2+d2(n+1)^2;
+              
+       n=n+1;       
+   end
    
    figure(1);
    hold on;
    plot(u1, y, '*');
    plot(u2, y, '*');
    figure(2);
-   plot(d, '-b');
+   plot(d1);
+   plot(d2);
 end
 
